a0c6a41568717f57608c43e6e1f1e7eb374c35d3
Xiaodong <403533049@qq.com>
2020-05-23 19:45:46 +0800

    comma code

---
+++ commacode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+def list_str(alist):
+	alist[-1] = 'and ' + alist[-1]
+	astring = ''
+	for item in alist:
+		astring = astring + item + ', '
+
+	print(astring[:-2])
+
+
+
+list_str(spam)
