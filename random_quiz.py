4ad60066b6c4aca7e00b15872f05ec59c8b1e0c7
Xiaodong <403533049@qq.com>
2020-05-24 22:43:26 +0800

    random quiz

---
+++ random_quiz.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import random
+
+capitals = {'Alabama':'Montogomery', 'Alaska':'Juneau',
+'Georgia':'Atlanta','Montana':'Helena','New Jersey':'Trenton'}
+
+
+for quizNum in range(5):
+	quizFile = open('capitalsquiz%s.txt' %(quizNum+1), 'w')
+	answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+	quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+	quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+	quizFile.write('\n\n')
+
+	states = list(capitals.keys())
+	random.shuffle(states)
+	for questionNum in range(3):
+		correctAnswer = capitals[states[questionNum]]
+		wrongAnswers = list(capitals.values())
+		del wrongAnswers[wrongAnswers.index(correctAnswer)]
+		wrongAnswers = random.sample(wrongAnswers,3)
+		answerOptions = wrongAnswers + [correctAnswer]
+		random.shuffle(answerOptions)
+		quizFile.write(' %s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+		for i in range(4):
+			quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+			quizFile.write('\n')
+			answerKeyFile.write('%s, %s\n' % (questionNum + 1, 'ABCD'[
+			answerOptions.index(correctAnswer)]))
+quizFile.close()
+answerKeyFile.close()
+
+
+
+
+
+
+
+
