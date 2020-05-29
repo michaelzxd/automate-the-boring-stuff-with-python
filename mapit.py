099afca50a224fdbb749c66d3a7c6facd27f8641
Xiaodong <403533049@qq.com>
2020-05-29 22:30:40 +0800

    request application

---
+++ mapit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import webbrowser, sys,pyperclip
+if len(sys.argv) > 1:
+	address = ' '.join(sys.argv[1:])
+else:
+	address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
