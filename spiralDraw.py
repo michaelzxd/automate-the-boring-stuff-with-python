3a3f2b640114ea23fd43699a821bfdb8831b8dea
Xiaodong <403533049@qq.com>
2020-06-04 21:50:25 +0800

    draw

---
+++ spiralDraw.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import pyautogui, time
+time.sleep(5)
+pyautogui.click()
+distance = 100
+while distance > 0:
+	pyautogui.dragRel(distance, 300, duration=0.2)
+	distance = distance - 5
+	pyautogui.dragRel(300,distance, duration=0.2)
+	pyautogui.dragRel(-distance, 300, duration=0.2)
+	distance = distance - 5
+	pyautogui.dragRel(300,-distance, duration=0.2)
