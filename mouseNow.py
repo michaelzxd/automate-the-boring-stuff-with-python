5de4879a130e0b56373c14b0964ec36cdff95841
Xiaodong <403533049@qq.com>
2020-06-06 10:30:27 +0800

    mousenow

--- mouseNow.py
+++ mouseNow.py
@@ -8,6 +8,14 @@ try:
 	while True:
 		x,y = pyautogui.position()
 		positionStr = 'X: ' + str(x).rjust(4) + str(y).rjust(4)
+		pixelColor = pyautogui.screenshot().getpixel((x,y))
+		positionStr += ' RGB: (' + str(pixelColor[0]).rjust(3)
+		positionStr += ' ,' + str(pixelColor[1]).rjust(3)
+		positionStr += ' ,' + str(pixelColor[2]).rjust(3) + ')'
+
+
+
+
 		print(positionStr, end='')
 		print('\b' * len(positionStr), end='',flush=True)
 except KeyboardInterrupt:
--- spiralDraw.py
+++ spiralDraw.py
@@ -4,6 +4,8 @@
 import pyautogui, time
 time.sleep(5)
 pyautogui.click()
+
+
 distance = 100
 while distance > 0:
 	pyautogui.dragRel(distance, 300, duration=0.2)
